7ac61d67b6b8c004cdd3642b505350bc5d321e4e
Vikas Shukla <vikasofficial927@gmail.com>
2025-06-15 16:54:22 -0400

    Sliding Window

---
+++ Sliding/substring Anagram.py
@@ -0,0 +1,33 @@
+# def substring_anagram(s, t):
+#     len_s = len(s)
+#     len_t = len(t)
+
+#     if len_t > len_s:
+#         return 0
+    
+#     count = 0
+
+#     expected_freq = [0] * 26
+#     window_freq = [0] * 26
+
+#     for c in t:
+#         expected_freq[ord(c) - ord('a')] += 1
+
+#         left = right = 0
+
+#     print(expected_freq)
+
+
+# if __name__ == '__main__':
+#     s = "caabab"
+#     t = "aba"
+#     substring_anagram(s, t)
+
+#     # print(f"Last word: {last}, count: {count}")
+
+
+
+print(ord('b') - ord('c'))
+
+
+
